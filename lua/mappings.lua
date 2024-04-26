223420cda0d75729d5fd135e31fe688ce11ae764
Care <eskil.skotvold@gmail.com>
2024-04-26 15:01:59 +0200

    added copilot

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,11 +2,20 @@ require "nvchad.mappings"
 
 local inlay_hints = require "configs/inlay_hints"
 local map =vim.keymap.set
+-- local opts = { noremap = true}
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
+-- Github Copilot
+map("n", "<leader>cpe", 'copilot#Enable()', { noremap = true, silent = true, desc = "Enable copilot" })
+map("n", "<leader>cpd", 'copilot#Disable()', { noremap = true, silent = true, desc = "Disable copilot" })
+map("n", "<leader>cpn", 'copilot#Next()', { noremap = true, silent = true, expr = true, desc = "Next copilot suggestion" })
+map("i", "<leader>cp", 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true, desc = "Accept copilot suggestion" })
+
+-- Visual block mode
+map("n", "vm", "<C-v>", { noremap = true, silent = true, desc = "visual block mode setup" })
 
 -- toggle global custom functions
 map('n', '<leader>td', '<cmd>lua toggle_diagnostics_in_insert_mode()<CR>', {
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -79,7 +79,7 @@ return {
    	},
   },
 
-   {
+  {
    	"nvim-treesitter/nvim-treesitter",
    	opts = {
    		ensure_installed = {
@@ -108,7 +108,18 @@ return {
         "markdown_inline"
    		},
    	},
-   },
+  },
 
 
+{
+  "github/copilot.vim",
+  lazy = false,
+  config = function()  -- Mapping tab is already used by NvChad
+    vim.g.copilot_no_tab_map = true;
+    vim.g.copilot_assume_mapped = true;
+    vim.g.copilot_tab_fallback = "";
+  -- The mapping is set to other key, see custom/lua/mappings
+  -- or run <leader>ch to see copilot mapping section
+  end
+  },
 }
